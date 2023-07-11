17626d688fac32743a2f9bcd2c6a384ae2ecfd75
Kim Tae Wook <gmill0411@naver.com>
2023-07-11 18:02:37 +0900

    김태욱 : 라이브러리 추가 방법 적용 및 확인

---
+++ KTW_DX/App/App.cpp
@@ -0,0 +1,20 @@
+﻿// App.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#pragma comment(lib, "GameEngineBase.lib")
+#pragma comment(lib, "GameEnginePlatform.lib")
+
+#include <Windows.h>
+
+#include <GameEnginePlatform/GameEngineWindow.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	GameEngineWindow::MainWindow.Open("Test", hInstance);
+	GameEngineWindow::MainWindow.SetPosAndScale({ 100, 100 }, { 500, 500 });
+
+	GameEngineWindow::MessageLoop(hInstance, nullptr, nullptr, nullptr);
+}
