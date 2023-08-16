9fa3044268712eac47efe695a97536feed9c6f2e
TaeWook <ktwisland111@gmail.com>
2023-08-16 10:54:18 +0900

    김태욱 : Test Contents

---
+++ KTW_DX/GameEngineContents/TitleLevel.cpp
@@ -0,0 +1,29 @@
+﻿#include "PreCompile.h"
+#include "ContentsCore.h"
+#include "TitleLevel.h"
+
+
+TitleLevel::TitleLevel()
+{
+}
+
+TitleLevel::~TitleLevel()
+{
+}
+
+
+void TitleLevel::Start()
+{
+	GetMainCamera()->Transform.SetLocalPosition({ 0.0f, 0.0f, -500.0f });
+	GetMainCamera()->SetProjectionType(EPROJECTIONTYPE::Perspective);
+
+	// std::shared_ptr<Player> NewPlayer = CreateActor<Player>();
+
+	// GetMainCamera()->SetParent(NewPlayer);
+	// CreateActor<GameEngineRenderer>();
+}
+
+void TitleLevel::Update(float _Delta)
+{
+	int a = 0;
+}
---
+++ KTW_DX/GameEngineContents/TitleLevel.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <GameEngineCore/GameEngineLevel.h>
+
+class TitleLevel : public GameEngineLevel
+{
+public:
+	// constrcuter destructer
+	TitleLevel();
+	~TitleLevel();
+
+	// delete Function
+	TitleLevel(const TitleLevel& _Other) = delete;
+	TitleLevel(TitleLevel&& _Other) noexcept = delete;
+	TitleLevel& operator=(const TitleLevel& _Other) = delete;
+	TitleLevel& operator=(TitleLevel&& _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+
+	void Update(float _Delta) override;
+
+private:
+
+};
