7bca1ff6e4c78e83d3aa5257d4d6007cd9239cc7
TaeWook <ktwisland111@gmail.com>
2023-09-04 12:09:48 +0900

    김태욱 : PlayLevel에 Test용 맵 렌더

--- KTW_DX/GameEngineContents/CupHeadEnum.h
+++ KTW_DX/GameEngineContents/CupHeadEnum.h
@@ -2,6 +2,7 @@ enum class UpdateOrder
 {
 	Player,
 	Monster,
+	Map,
 	BackGround
 };
 
--- KTW_DX/GameEngineContents/PlayLevel.cpp
+++ KTW_DX/GameEngineContents/PlayLevel.cpp
@@ -3,6 +3,7 @@
 #include "ContentsCore.h"
 
 #include "BaseCharacter.h"
+#include "Map.h"
 
 PlayLevel::PlayLevel()
 {
@@ -15,6 +16,10 @@ PlayLevel::~PlayLevel()
 
 void PlayLevel::Start()
 {
+	float4 WinScaleHalf = GameEngineCore::MainWindow.GetScale().Half();
+	GetMainCamera()->Transform.SetLocalPosition({ WinScaleHalf.X, -WinScaleHalf.Y, -500 });
+	GetMainCamera()->SetProjectionType(EPROJECTIONTYPE::Orthographic);
+
 	{
 		GameEngineDirectory Dir;
 		Dir.MoveParentToExistsChild("Resources");
@@ -28,8 +33,20 @@ void PlayLevel::Start()
 			GameEngineSprite::CreateFolder(Dir.GetStringPath());
 		}
 	}
+	{
+		GameEnginePath Path;
+		Path.MoveParentToExistsChild("Resources");
+		Path.MoveChild("Resources\\Texture\\Map\\TestMap.Png");
+		GameEngineTexture::Load(Path.GetStringPath());
+		GameEngineSprite::CreateSingle("TestMap.Png");
+	}
 
 	std::shared_ptr<BaseCharacter> NewPlayer = CreateActor<BaseCharacter>(UpdateOrder::Player);
+	NewPlayer->Transform.SetLocalPosition({ WinScaleHalf.X, 0.0f });
+
+	std::shared_ptr<Map> TestMap = CreateActor<Map>(UpdateOrder::Map);
+	TestMap->MapInit("TestMap.Png");
+	TestMap->Transform.SetLocalPosition({ WinScaleHalf.X, -WinScaleHalf.Y });
 }
 
 void PlayLevel::Update(float _Delta)
