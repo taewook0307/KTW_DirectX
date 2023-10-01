afd310316c9737f1d6c09c282ef8d1e55490cea2
Kim Tae Wook <gmill0411@naver.com>
2023-10-01 11:41:03 +0900

    김태욱 : 엔진 업데이트(벡터 나머지 연산 추가)

--- KTW_DX/GameEngineBase/GameEngineMath.h
+++ KTW_DX/GameEngineBase/GameEngineMath.h
@@ -248,6 +248,16 @@ public:
 		return ReturnValue;
 	}
 
+
+	float4 operator%(const float4 _Value) const
+	{
+		float4 ReturnValue = DirectX::XMVectorMod(DirectXVector, _Value.DirectXVector);
+		ReturnValue.W = W;
+
+		return ReturnValue;
+	}
+
+
 	float4& operator/=(const float4 _Value)
 	{
 		float PrevW = W;
