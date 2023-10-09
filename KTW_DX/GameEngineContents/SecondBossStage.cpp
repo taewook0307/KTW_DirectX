8824e78e1a957db5e7d68c49c38598c9ea569cb4
Kim Tae Wook <gmill0411@naver.com>
2023-10-09 21:26:08 +0900

    김태욱 : 업데이트

--- KTW_DX/GameEngineContents/SecondBossStage.cpp
+++ KTW_DX/GameEngineContents/SecondBossStage.cpp
@@ -71,10 +71,10 @@ void SecondBossStage::LevelStart(GameEngineLevel* _PrevLevel)
 	float4 WinScale = GameEngineCore::MainWindow.GetScale();
 
 	PirateBossActor = CreateActor<PirateBoss>(EUPDATEORDER::Monster);
-	PirateBossActor->Transform.SetLocalPosition({ WinScale.X - 80.0f, -470.0f });
+	PirateBossActor->Transform.SetLocalPosition({ WinScale.X - 80.0f, -450.0f });
 
 	ShipBossActor = CreateActor<ShipBoss>(EUPDATEORDER::Monster);
-	ShipBossActor->Transform.SetLocalPosition({ WinScale.X + 70.0f, -WinScale.Y });
+	ShipBossActor->Transform.SetLocalPosition({ WinScale.X + 80.0f, -WinScale.Y });
 
 	Player = CreateActor<BaseCharacter>(EUPDATEORDER::Player);
 	Player->Transform.SetLocalPosition({ 230.0f, -677.0f });
